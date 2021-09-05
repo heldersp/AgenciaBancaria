013a8d3804a2a76c52eddfdfb117f997e3442520
helde <helde@DESKTOP-C1RKKFS>
2021-09-05 17:38:28 -0300

    Adicionar arquivos de projeto.

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AgenciaBancaria.Dominio;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua teste", "123456789", "Mauá", "SP");
+                Cliente cliente = new Cliente("Helder", "123456", "456789", endereco);
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                string senha = "as455645645633";
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Sando: " + conta.Saldo);
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         }
+    }
+}
